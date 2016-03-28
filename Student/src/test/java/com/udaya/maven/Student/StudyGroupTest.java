d53d96bd9722364113f509e214cc124b24245392
Udayasree_Patlola <Udayasree_Patlola@infosys.com>
2016-03-28 22:31:33 +0530

    First commit

---
+++ Student/src/test/java/com/udaya/maven/Student/StudyGroupTest.java
@@ -0,0 +1,38 @@
+package com.udaya.maven.Student;
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StudyGroupTest {
+
+	@Test
+	public void testStudyGroupClass() {
+        // prepare the data for test
+        final List<Student> testStudents = new ArrayList<Student>();
+        testStudents.add(new Student(1, "Student No.1", 60));
+        testStudents.add(new Student(2, "Student No.2", 70));
+        testStudents.add(new Student(3, "Student No.2", 80));
+        
+        // Start the unit test
+        final StudyGroup testGroup = new StudyGroup();
+        for(Student student: testStudents) {
+            testGroup.addStudent(student);
+        }
+        assertEquals(testGroup.getGroupSize(), testStudents.size());
+        
+        Student testStudent = testStudents.get(0);
+        Student returnedStudent = testGroup.getStudent(testStudent.getId());
+        assertSame(returnedStudent, testStudent);
+        assertEquals(returnedStudent.getId(), testStudent.getId());
+        assertEquals(returnedStudent.getName(), testStudent.getName());
+        assertEquals(returnedStudent.getScore(), testStudent.getScore());
+        
+        testGroup.removeStudent(testStudent.getId());
+        assertEquals(testGroup.getGroupSize(), testStudents.size() - 1);
+        
+        testGroup.clear();
+        assertEquals(testGroup.getGroupSize(), 0);
+    }
+}
