e42eaa78ada40b2668dcc2e4801efecf32a18aa3
Udayasree_Patlola <Udayasree_Patlola@infosys.com>
2016-03-31 22:25:42 +0530

    selenium t3

--- Student/src/test/java/com/udaya/maven/Student/showClose.java
+++ Student/src/test/java/com/udaya/maven/Student/showClose.java
@@ -4,16 +4,19 @@ package com.udaya.maven.Student;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class showClose {
-	WebDriver driver = new FirefoxDriver();
+	
 	
 	@Test 
 	
 	public void test() throws InterruptedException { 
+	System.setProperty("webdriver.chrome.driver", "D://chromedriver.exe");
+	WebDriver driver = new ChromeDriver();
 	driver.manage().window().maximize(); 
 	driver.get("http://only-testing-blog.blogspot.in/2013/11/new-test.html"); 
 	driver.findElement(By.xpath("//input[@name='fname']")).sendKeys("junittest1 executed"); 
